134545b338ad9c0d4d2dc86dde1ca9fc28dcc23e
Nitish Mhaske <35861324+NitishMhaske@users.noreply.github.com>
2021-09-01 21:35:32 +0530

    Create EqualSumChecker.java

---
+++ EqualSumChecker.java
@@ -0,0 +1,11 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int on,int tw,int th){
+        if((on+tw)==th){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+ 
+}
